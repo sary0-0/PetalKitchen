9b7d9506ef2f82a3e553b11c23439a15fe5569a4
Sary <sara.omerovic@cetvrta-gimnazija.edu.ba>
2026-04-24 01:17:22 +0200

    Coolinarka - finalna verzija

--- server.js
+++ server.js
@@ -122,7 +122,7 @@ app.get('/api/shopping-lista/:userId', (req, res) => {
     });
 });
 
-app.get('*', (req, res) => {
+app.get((req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
